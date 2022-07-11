7545795376fe06e15b6d13bf2c3bb7a2101a62d0
Muhammad Rizkillah <65638961+mrizklh@users.noreply.github.com>
2022-07-11 08:04:12 +0000

    "males"

--- site.config.ts
+++ site.config.ts
@@ -2,26 +2,25 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: "92731583beb44f03b000d0d98fe44ab9",
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
-  rootNotionSpaceId: null,
+  rootNotionSpaceId: "f8b943dc-6623-493c-af87-54ddbb55a5d6",
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Muhammad Rizkillah',
+  domain: 'www.rizki.me',
+  author: 'Muhammad Rizkillah',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
-
+  description: 'Muhammad Rizkillah meruapakan seorang mahasiswa Universitas Indonesia (UI), www.rizki.me merupakan personal website sekaligus berita terbaru tentang teknologi, blog.rizki.me merupakan dokumentasi dari hasil belajar Muhammad Rizkillah.',
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
-  // newsletter: '#', // optional newsletter URL
-  // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
+  twitter: 'mrizklh',
+  github: 'mrizklh',
+  linkedin: 'mrizklh',
+  newsletter: 'medium.com/narasi', // optional newsletter URL
+  youtube: 'channel/UCsN2cgzKBFkCOG_XsnTtiBQ', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
@@ -36,29 +35,35 @@ export default siteConfig({
   // NOTE: if you enable redis, you need to set the `REDIS_HOST` and `REDIS_PASSWORD`
   // environment variables. see the readme for more info
   isRedisEnabled: false,
+  
 
   // map of notion page IDs to URL paths (optional)
   // any pages defined here will override their default URL paths
   // example:
   //
-  // pageUrlOverrides: {
-  //   '/foo': '067dd719a912471ea9a3ac10710e7fdf',
-  //   '/bar': '0be6efce9daf42688f65c76b89f8eb27'
-  // }
-  pageUrlOverrides: null,
+  pageUrlOverrides: {
+     '/News': '289b8a386d2b4578bdb6b2a33e074181',
+
+     '/learn': 'b9fbf5bbb87645fb91866bc5aa766a40'
+  },
+
+
+//  pageUrlOverrides: null,
 
   // whether to use the default notion navigation style or a custom one with links to
   // important pages
-  navigationStyle: 'default'
-  // navigationStyle: 'custom',
-  // navigationLinks: [
-  //   {
-  //     title: 'About',
-  //     pageId: 'f1199d37579b41cbabfc0b5174f4256a'
-  //   },
-  //   {
-  //     title: 'Contact',
-  //     pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
-  //   }
-  // ]
+  //navigationStyle: 'default'
+  navigationStyle: 'custom',
+  navigationLinks: [
+    {
+      title: "News",
+      pageId: '289b8a386d2b4578bdb6b2a33e074181',
+
+    },
+    {
+      title: 'learn',
+      pageId: 'b9fbf5bbb87645fb91866bc5aa766a40',
+
+    }
+ ]
 })
